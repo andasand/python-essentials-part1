1432a47eb741e53646c50a7fe4015a6081241c89
andasand <anand.sandhinti@gmail.com>
2021-11-15 09:09:01 -0500

    module 1 completed

---
+++ module1/2-simple-python-script.py
@@ -0,0 +1,14 @@
+print("Hissssss...")
+#print("Hisssss....."   # End of File error
+#rint("Hisssss.....")    # NameError: name 'rint' is not defined
+
+'''
+Traceback (most recent call last):
+  File "c:\Users\Anand Sandhinti\Documents\GitHub\python-essentials-part1\module1\2-simple-python-script.py", line 3, in <module>
+    rint("Hisssss.....")    # 
+NameError: name 'rint' is not defined
+'''
+# traceback --> path that the code traverses through
+# location of the error --> name of the file, line #, module name
+# content of the erroneous line
+# name of the error
