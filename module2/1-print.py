b3eb7564aa97b0efcb3f5f06dbc33de7bb83bc49
andasand <anand.sandhinti@gmail.com>
2021-11-15 19:27:18 -0500

    commiting for the day

---
+++ module2/1-print.py
@@ -0,0 +1,25 @@
+print("Hello, World!")
+
+# the word print is a function name
+'''
+A function 
+1. causes some effect
+    a. send text to the terminal
+    b. create a file
+    c. draw an image
+    d. play a sound
+2. evaluate a value and return it as the function's result
+    a. square root of a value
+    b. length of a given text
+'''
+
+# where do functions come from?
+# 1. from python itself --> built-in
+# 2. from modules 
+# 3. written by myself
+
+# name of the function should be significant
+
+# functions take in arguments (as many as necessary to perform their tasks)
+# python functions strongly demand the prescence of a pair of parentheses - opening and closing ones
+# arguments are placed inside the parentheses
